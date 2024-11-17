4e10b00030f04abba409c1be2ba7cf6f14377a99
Doncan00 <90412543+Doncan00@users.noreply.github.com>
2024-11-17 03:51:06 -0700

    user cards actualizadas

--- views/clients/index.php
+++ views/clients/index.php
@@ -4,33 +4,34 @@ include_once "../../app/config.php";
 
 <!doctype html>
 <html lang="en">
-  <!-- [Head] start -->
+<!-- [Head] start -->
 
-  <head>
+<head>
   <?php
 
-include "../layouts/head.php";
+  include "../layouts/head.php";
 
-?>
-  </head>
-  <!-- [Head] end -->
-  <!-- [Body] Start -->
+  ?>
+</head>
+<!-- [Head] end -->
+<!-- [Body] Start -->
 
-  <body>
+<body>
+
   <body data-pc-preset="preset-1" data-pc-sidebar-theme="light" data-pc-sidebar-caption="true" data-pc-direction="ltr" data-pc-theme="light">
 
 
-<?php
+    <?php
 
-include "../layouts/sidebar.php";
+    include "../layouts/sidebar.php";
 
-?>
+    ?>
 
-<?php
+    <?php
 
-include "../layouts/nav.php";
+    include "../layouts/nav.php";
 
-?>
+    ?>
 
 
 
@@ -62,10 +63,35 @@ include "../layouts/nav.php";
         <!-- [ Main Content ] start -->
         <div class="row">
           <!-- [ sample-page ] start -->
-          <div class="col-md-6 col-xl-4">
+          <div class="col-md-6 col-xl-4" style="max-width: 350px;">
             <div class="card user-card">
-              
-            <div class="card-body">
+              <div class="card-body">
+                <div class="user-cover-bg">
+                  <img src="<?= BASE_PATH ?>/assets/images/application/img-user-cover-1.jpg" alt="image" class="img-fluid" />
+                </div>
+                <div class="chat-avtar card-user-image">
+                  <img src="<?= BASE_PATH ?>/assets/images/user/avatar-1.jpg" alt="user-image" class="img-thumbnail rounded-circle" />
+                  <i class="chat-badge bg-success"></i>
+                </div>
+                <div class="d-flex">
+                  <div class="flex-grow-1 ms-2">
+                    <h6 class="mb-1">Marshall Parker</h6>
+                    <p class="text-muted text-sm mb-0">Email: <a href="mailto:mapa_46@gmail.com" class="text-primary">mapa_46@gmail.com</a></p>
+                    <p class="text-muted text-sm mb-0">Phone: 6127384765</p>
+                  </div>
+                </div>
+                <div class="saprator my-2">
+                  <span>Level</span>
+                </div>
+                <div class="text-center">
+                  <span class="badge bg-light-secondary border rounded-pill border-secondary bg-transparent f-14 me-1 mt-1">Normal (0% Discount)</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-xl-4" style="max-width: 350px;">
+            <div class="card user-card">
+              <div class="card-body">
                 <div class="user-cover-bg">
                   <img src="<?= BASE_PATH ?>/assets/images/application/img-user-cover-1.jpg" alt="image" class="img-fluid" />
                 </div>
@@ -95,13 +121,14 @@ include "../layouts/nav.php";
       </div>
     </div>
     <!-- [ Main Content ] end -->
-  <?php
+    <?php
 
-include "../layouts/footer.php";
-include "../layouts/scripts.php";
+    include "../layouts/footer.php";
+    include "../layouts/scripts.php";
 
-?>
+    ?>
 
   </body>
   <!-- [Body] end -->
+
 </html>
