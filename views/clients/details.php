4cfc5bea96ba69463441473983549c54d849c5ea
Doncan00 <90412543+Doncan00@users.noreply.github.com>
2024-11-17 20:16:13 -0700

    Profile client con datos del cliente

--- views/clients/details.php
+++ views/clients/details.php
@@ -63,179 +63,182 @@ include_once "../../app/config.php";
         <div class="row">
           <!-- [ sample-page ] start -->
           <div class="col-sm-12">
-            <div class="card social-profile">
-              <img src="<?= BASE_PATH ?>/assets/images/application/img-profile-cover.jpg" alt="" class="w-100 card-img-top" />
-              <div class="card-body pt-0">
-                <div class="row align-items-end">
-                  <div class="col-md-auto text-md-start">
-                    <img class="img-fluid img-profile-avtar" src="<?= BASE_PATH ?>/assets/images/user/avatar-5.jpg" alt="User image" />
-                  </div>
-                  <div class="col">
-                    <div class="row justify-content-between align-items-end">
-                      <div class="col-md-5 col-xl-6 soc-profile-data">
-                        <!-- DATOS PERSONALES -->
-                        <h5 class="mb-1">Stebin Ben</h5>
-                        <p class="mb-0">UI/UX Designer</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+  <div class="card social-profile">
+    <img src="<?= BASE_PATH ?>/assets/images/application/img-profile-cover.jpg" alt="" class="w-100 card-img-top" />
+    <div class="card-body pt-0">
+      <div class="row align-items-end">
+        <!-- Imagen del cliente -->
+        <div class="col-md-auto text-md-start">
+          <img class="img-fluid img-profile-avtar" src="<?= BASE_PATH ?>/assets/images/user/avatar-5.jpg" alt="User image" />
+        </div>
+        <!-- Datos personales del cliente -->
+        <div class="col">
+          <div class="d-flex flex-wrap align-items-center justify-content-start soc-profile-data">
+            <p class="me-3 mb-0"><strong>Nombre:</strong> Marshall Parker</p>
+            <p class="me-3 mb-0"><strong>Email:</strong> <i class="ph-envelope me-1"></i> mapa_46@gmail.com</p>
+            <p class="me-3 mb-0"><strong>Teléfono:</strong> <i class="ph-phone me-1"></i> 6127384765</p>
+            <p class="me-3 mb-0"><strong>Nivel:</strong> <i class="ph-star me-1"></i> Normal (Descuento: 0%)</p>
+            <p class="mb-0"><strong>Status:</strong> <span class="badge bg-success">Suscrito</span></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+          <div class="card">
+            <div class="card-body py-0">
+              <ul class="nav nav-tabs orders-tabs" id="myTab" role="tablist">
+                <li class="nav-item">
+                  <a class="nav-link active" id="orders-tab" data-bs-toggle="tab" href="#orders" role="tab" aria-selected="true">
+                    <i class="ph-duotone ph-list me-2"></i> Ordenes
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" id="compras-tab" data-bs-toggle="tab" href="#compras" role="tab" aria-selected="false">
+                    <i class="ph-duotone ph-shopping-cart me-2"></i> Total Compras
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" id="addresses-tab" data-bs-toggle="tab" href="#addresses" role="tab" aria-selected="false">
+                    <i class="ph-duotone ph-map-pin me-2"></i> Direcciones
+                  </a>
+                </li>
+              </ul>
             </div>
-            <div class="card">
-              <div class="card-body py-0">
-                <ul class="nav nav-tabs orders-tabs" id="myTab" role="tablist">
-                  <li class="nav-item">
-                    <a class="nav-link active" id="orders-tab" data-bs-toggle="tab" href="#orders" role="tab" aria-selected="true">
-                      <i class="ph-duotone ph-list me-2"></i> Ordenes
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" id="compras-tab" data-bs-toggle="tab" href="#compras" role="tab" aria-selected="false">
-                      <i class="ph-duotone ph-shopping-cart me-2"></i> Total Compras
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" id="addresses-tab" data-bs-toggle="tab" href="#addresses" role="tab" aria-selected="false">
-                      <i class="ph-duotone ph-map-pin me-2"></i> Direcciones
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-lg-12 col-xxl-12">
-                <!-- ORDERS -->
-                <div class="tab-content">
-                  <div class="tab-pane show active" id="orders" role="tabpanel">
-                    <div class="card">
-                      <div class="card-body">
-                        <div class="container my-3">
-                          <div class="row">
-                            <!-- Order 1 -->
-                            <div class="col-xxl-3 col-lg-4 col-sm-6">
-                              <div class="border card">
-                                <div class="p-2 card-body">
-                                  <h6 class="mb-2">Folio: 82712</h6>
-                                  <ul class="list-group list-group-flush my-2">
-                                    <li class="list-group-item px-0 py-2 d-flex justify-content-between">
-                                      <span>Total:</span>
-                                      <strong>$8999.99</strong>
-                                    </li>
-                                    <li class="list-group-item px-0 py-2 d-flex justify-content-between">
-                                      <span>Status:</span>
-                                      <span>Pendiente de pago</span>
-                                    </li>
-                                    <li class="list-group-item px-0 py-2 d-flex justify-content-between">
-                                      <span>Payment:</span>
-                                      <span>Efectivo</span>
-                                    </li>
-                                    <li class="list-group-item px-0 py-2 d-flex justify-content-between">
-                                      <span>Productos:</span>
-                                      <span>2</span>
-                                    </li>
-                                    <li class="list-group-item px-0 py-2 d-flex justify-content-between">
-                                      <span>Peso total:</span>
-                                      <span>15,000 g</span>
-                                    </li>
-                                    <li class="list-group-item px-0 py-2">
-                                      <strong>Productos incluidos:</strong>
-                                      <ul class="mb-0 ps-3">
-                                        <li>Comedor Miguel con 4 Sillas - $450</li>
-                                        <li>Colchón Matrimonial Zero - $8999.99</li>
-                                      </ul>
-                                    </li>
-                                    <li class="list-group-item px-0 py-2">
-                                      <strong>Dirección del cliente:</strong>
-                                      <p class="mb-0 ps-3">
-                                        Calle articulo 743, 123, La Paz, Baja California Sur, 23088
-                                      </p>
-                                    </li>
-                                    <li class="list-group-item px-0 py-2 d-flex justify-content-between">
-                                      <span>Cupón aplicado:</span>
-                                      <span>10% off</span>
-                                    </li>
-                                  </ul>
-                                </div>
+          </div>
+          <div class="row">
+            <div class="col-lg-12 col-xxl-12">
+              <!-- ORDERS -->
+              <div class="tab-content">
+                <div class="tab-pane show active" id="orders" role="tabpanel">
+                  <div class="card">
+                    <div class="card-body">
+                      <div class="container my-3">
+                        <div class="row">
+                          <!-- Order 1 -->
+                          <div class="col-xxl-3 col-lg-4 col-sm-6">
+                            <div class="border card">
+                              <div class="p-2 card-body">
+                                <h6 class="mb-2">Folio: 82712</h6>
+                                <ul class="list-group list-group-flush my-2">
+                                  <li class="list-group-item px-0 py-2 d-flex justify-content-between">
+                                    <span>Total:</span>
+                                    <strong>$8999.99</strong>
+                                  </li>
+                                  <li class="list-group-item px-0 py-2 d-flex justify-content-between">
+                                    <span>Status:</span>
+                                    <span>Pendiente de pago</span>
+                                  </li>
+                                  <li class="list-group-item px-0 py-2 d-flex justify-content-between">
+                                    <span>Payment:</span>
+                                    <span>Efectivo</span>
+                                  </li>
+                                  <li class="list-group-item px-0 py-2 d-flex justify-content-between">
+                                    <span>Productos:</span>
+                                    <span>2</span>
+                                  </li>
+                                  <li class="list-group-item px-0 py-2 d-flex justify-content-between">
+                                    <span>Peso total:</span>
+                                    <span>15,000 g</span>
+                                  </li>
+                                  <li class="list-group-item px-0 py-2">
+                                    <strong>Productos incluidos:</strong>
+                                    <ul class="mb-0 ps-3">
+                                      <li>Comedor Miguel con 4 Sillas - $450</li>
+                                      <li>Colchón Matrimonial Zero - $8999.99</li>
+                                    </ul>
+                                  </li>
+                                  <li class="list-group-item px-0 py-2">
+                                    <strong>Dirección del cliente:</strong>
+                                    <p class="mb-0 ps-3">
+                                      Calle articulo 743, 123, La Paz, Baja California Sur, 23088
+                                    </p>
+                                  </li>
+                                  <li class="list-group-item px-0 py-2 d-flex justify-content-between">
+                                    <span>Cupón aplicado:</span>
+                                    <span>10% off</span>
+                                  </li>
+                                </ul>
                               </div>
                             </div>
+                          </div>
 
-                            <!-- Order 2 -->
-                            <div class="col-xxl-3 col-lg-4 col-sm-6">
-                              <div class="border card">
-                                <div class="p-2 card-body">
-                                  <h6 class="mb-2">Folio: 82718</h6>
-                                  <ul class="list-group list-group-flush my-2">
-                                    <li class="list-group-item px-0 py-2 d-flex justify-content-between">
-                                      <span>Total:</span>
-                                      <strong>$9599.97</strong>
-                                    </li>
-                                    <li class="list-group-item px-0 py-2 d-flex justify-content-between">
-                                      <span>Status:</span>
-                                      <span>Cancelada</span>
-                                    </li>
-                                    <li class="list-group-item px-0 py-2 d-flex justify-content-between">
-                                      <span>Payment:</span>
-                                      <span>Tarjeta</span>
-                                    </li>
-                                    <li class="list-group-item px-0 py-2 d-flex justify-content-between">
-                                      <span>Productos:</span>
-                                      <span>3</span>
-                                    </li>
-                                    <li class="list-group-item px-0 py-2 d-flex justify-content-between">
-                                      <span>Peso total:</span>
-                                      <span>10,810 g</span>
-                                    </li>
-                                    <li class="list-group-item px-0 py-2">
-                                      <strong>Productos incluidos:</strong>
-                                      <ul class="mb-0 ps-3">
-                                        <li>Tostador Record 2 Rebanadas - $299.99</li>
-                                        <li>Escurridor de Acero para Platos Farberware - $6299.99</li>
-                                        <li>AirPods Pro con Estuche de Carga - $2999.99</li>
-                                      </ul>
-                                    </li>
-                                    <li class="list-group-item px-0 py-2">
-                                      <strong>Dirección del cliente:</strong>
-                                      <p class="mb-0 ps-3">
-                                        Calle Lope de Rueda, 32, Cabanillas del Campo, Guadalajara, 19171
-                                      </p>
-                                    </li>
-                                    <li class="list-group-item px-0 py-2 d-flex justify-content-between">
-                                      <span>Cupón aplicado:</span>
-                                      <span>15% off</span>
-                                    </li>
-                                  </ul>
-                                </div>
+                          <!-- Order 2 -->
+                          <div class="col-xxl-3 col-lg-4 col-sm-6">
+                            <div class="border card">
+                              <div class="p-2 card-body">
+                                <h6 class="mb-2">Folio: 82718</h6>
+                                <ul class="list-group list-group-flush my-2">
+                                  <li class="list-group-item px-0 py-2 d-flex justify-content-between">
+                                    <span>Total:</span>
+                                    <strong>$9599.97</strong>
+                                  </li>
+                                  <li class="list-group-item px-0 py-2 d-flex justify-content-between">
+                                    <span>Status:</span>
+                                    <span>Cancelada</span>
+                                  </li>
+                                  <li class="list-group-item px-0 py-2 d-flex justify-content-between">
+                                    <span>Payment:</span>
+                                    <span>Tarjeta</span>
+                                  </li>
+                                  <li class="list-group-item px-0 py-2 d-flex justify-content-between">
+                                    <span>Productos:</span>
+                                    <span>3</span>
+                                  </li>
+                                  <li class="list-group-item px-0 py-2 d-flex justify-content-between">
+                                    <span>Peso total:</span>
+                                    <span>10,810 g</span>
+                                  </li>
+                                  <li class="list-group-item px-0 py-2">
+                                    <strong>Productos incluidos:</strong>
+                                    <ul class="mb-0 ps-3">
+                                      <li>Tostador Record 2 Rebanadas - $299.99</li>
+                                      <li>Escurridor de Acero para Platos Farberware - $6299.99</li>
+                                      <li>AirPods Pro con Estuche de Carga - $2999.99</li>
+                                    </ul>
+                                  </li>
+                                  <li class="list-group-item px-0 py-2">
+                                    <strong>Dirección del cliente:</strong>
+                                    <p class="mb-0 ps-3">
+                                      Calle Lope de Rueda, 32, Cabanillas del Campo, Guadalajara, 19171
+                                    </p>
+                                  </li>
+                                  <li class="list-group-item px-0 py-2 d-flex justify-content-between">
+                                    <span>Cupón aplicado:</span>
+                                    <span>15% off</span>
+                                  </li>
+                                </ul>
                               </div>
                             </div>
                           </div>
                         </div>
-
                       </div>
                     </div>
                   </div>
-                  <!-- TOTAL DE COMPRAS -->
-                  <div class="tab-pane" id="compras" role="tabpanel" aria-labelledby="compras-tab">
-                    <div class="card">
-                      <div class="card-body">
-                      </div>
+                </div>
+                <!-- TOTAL DE COMPRAS -->
+                <div class="tab-pane" id="compras" role="tabpanel" aria-labelledby="compras-tab">
+                  <div class="card">
+                    <div class="card-body">
                     </div>
                   </div>
-                  <!-- DIRECCIONES -->
-                  <div class="tab-pane" id="addresses" role="tabpanel" aria-labelledby="addresses-tab">
-                    <div class="card">
-                      <div class="card-body">
-                      </div>
+                </div>
+                <!-- DIRECCIONES -->
+                <div class="tab-pane" id="addresses" role="tabpanel" aria-labelledby="addresses-tab">
+                  <div class="card">
+                    <div class="card-body">
                     </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <!-- [ sample-page ] end -->
         </div>
-        <!-- [ Main Content ] end -->
+        <!-- [ sample-page ] end -->
       </div>
+      <!-- [ Main Content ] end -->
+    </div>
     </div>
     <div class="modal fade modal-lightbox post-modal-lightbox" id="lightboxModal" tabindex="-1" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered">
@@ -290,3 +293,20 @@ include_once "../../app/config.php";
   <!-- [Body] end -->
 
 </html>
+
+<style>
+  .soc-profile-data {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1rem; /* Espacio entre elementos */
+  }
+
+  .soc-profile-data p {
+    margin: 0; /* Elimina márgenes adicionales */
+    font-size: 1rem; /* Ajusta el tamaño de fuente */
+  }
+
+  .soc-profile-data strong {
+    color: #333; /* Color de los encabezados */
+  }
+</style>
